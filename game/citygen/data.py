c5ce4700931e2df849262ab9953947521ac81360
Daniel Stokes <kupomail@gmail.com>
2015-03-20 22:12:23 -0700

    Refactor: Moving weighted_choice to citygen/util.py

--- game/citygen/data.py
+++ game/citygen/data.py
@@ -1,44 +1,44 @@
 class ResourceData(object):
-	__slots__ = ["weight", "color"]
+    __slots__ = ["weight", "color"]
 
-	def __init__(self, weight, color):
-		self.weight = weight
-		self.color = color
+    def __init__(self, weight, color):
+        self.weight = weight
+        self.color = color
 
 
 RESOURCES = {
-	"NONE": ResourceData(0.0, (32, 32, 32)),
-	"EMPTY": ResourceData(0.85, (170, 170, 170)),
-	"ALPHA": ResourceData(0.05, (170, 0, 0)),
-	"BETA": ResourceData(0.05, (0, 0, 170)),
-	"GAMMA": ResourceData(0.05, (0, 170, 0)),
+    "NONE": ResourceData(0.0, (32, 32, 32)),
+    "EMPTY": ResourceData(0.85, (170, 170, 170)),
+    "ALPHA": ResourceData(0.05, (170, 0, 0)),
+    "BETA": ResourceData(0.05, (0, 0, 170)),
+    "GAMMA": ResourceData(0.05, (0, 170, 0)),
 }
 
 
 class Building(object):
-	__slots__ = ["position", "mesh", "collision", "resource", "nodepath", "owner"]
+    __slots__ = ["position", "mesh", "collision", "resource", "nodepath", "owner"]
 
-	def __init__(self, position, mesh, collision, resource):
-		self.position = position
-		self.mesh = mesh
-		self.collision = collision
-		self.resource = resource
-		self.nodepath = None
-		self.owner = None
+    def __init__(self, position, mesh, collision, resource):
+        self.position = position
+        self.mesh = mesh
+        self.collision = collision
+        self.resource = resource
+        self.nodepath = None
+        self.owner = None
 
 
 class Mesh(object):
-	__slots__ = ["vertices", "faces", "material"]
+    __slots__ = ["vertices", "faces", "material"]
 
-	def __init__(self, vertices, faces, material=None):
-		self.vertices = vertices
-		self.faces = faces
-		self.material = material
+    def __init__(self, vertices, faces, material=None):
+        self.vertices = vertices
+        self.faces = faces
+        self.material = material
 
 
 class Vertex(object):
-	__slots__ = ["position", "normal"]
+    __slots__ = ["position", "normal"]
 
-	def __init__(self, position=(0.0, 0.0, 0.0), normal=(0.0, 0.0, 1.0)):
-		self.position = position
-		self.normal = normal
+    def __init__(self, position=(0.0, 0.0, 0.0), normal=(0.0, 0.0, 1.0)):
+        self.position = position
+        self.normal = normal
--- game/citygen/util.py
+++ game/citygen/util.py
@@ -2,31 +2,31 @@ import panda3d.core as p3d
 
 
 def mesh_to_p3d_node(mesh, name, material):
-		node = p3d.GeomNode(name)
+        node = p3d.GeomNode(name)
 
-		vdata = p3d.GeomVertexData(name,
-			p3d.GeomVertexFormat.get_v3n3(),
-			p3d.GeomEnums.UH_stream)
-		vdata.unclean_set_num_rows(len(mesh.vertices))
+        vdata = p3d.GeomVertexData(name,
+            p3d.GeomVertexFormat.get_v3n3(),
+            p3d.GeomEnums.UH_stream)
+        vdata.unclean_set_num_rows(len(mesh.vertices))
 
-		vwriter = p3d.GeomVertexWriter(vdata, 'vertex')
-		nwriter = p3d.GeomVertexWriter(vdata, 'normal')
-		for vert in mesh.vertices:
-			vwriter.add_data3(*vert.position)
-			nwriter.add_data3(*vert.normal)
-		vwriter = None
-		nwriter = None
+        vwriter = p3d.GeomVertexWriter(vdata, 'vertex')
+        nwriter = p3d.GeomVertexWriter(vdata, 'normal')
+        for vert in mesh.vertices:
+            vwriter.add_data3(*vert.position)
+            nwriter.add_data3(*vert.normal)
+        vwriter = None
+        nwriter = None
 
-		prim = p3d.GeomTriangles(p3d.GeomEnums.UH_stream)
-		for face in mesh.faces:
-			prim.add_vertices(*face)
+        prim = p3d.GeomTriangles(p3d.GeomEnums.UH_stream)
+        for face in mesh.faces:
+            prim.add_vertices(*face)
 
-		render_state = p3d.RenderState.make_empty()
+        render_state = p3d.RenderState.make_empty()
 
-		render_state = render_state.set_attrib(p3d.MaterialAttrib.make(material))
+        render_state = render_state.set_attrib(p3d.MaterialAttrib.make(material))
 
-		geom = p3d.Geom(vdata)
-		geom.add_primitive(prim)
-		node.add_geom(geom, render_state)
+        geom = p3d.Geom(vdata)
+        geom.add_primitive(prim)
+        node.add_geom(geom, render_state)
 
-		return node
+        return node
