938c49c09b31a7ac4f50dc04161d96f3cedd6ec8
Daniel Stokes <kupomail@gmail.com>
2015-03-21 22:10:50 -0700

    Map: Moving map updating into a task, and showing the player on the map

--- game/citygen/map.py
+++ game/citygen/map.py
@@ -15,7 +15,6 @@ uniform mat4 p3d_ModelViewProjectionMatrix;
 void main()
 {
     gl_Position = p3d_ModelViewProjectionMatrix * p3d_Vertex;
-
     var_barycentric = p3d_Normal;
 }
 """
@@ -44,16 +43,15 @@ class CityMap(object):
     def __init__(self, city):
         self.city = city
 
-        # self.alpha = 0.6
         self.base_np = p3d.NodePath("CityMap-Placeholder")
         self.base_np.set_hpr(p3d.VBase3(0, 90.0, 0))
         self.base_np.set_scale(0.9)
         self.base_np.set_transparency(1)
 
         shader = p3d.Shader.make(p3d.Shader.SL_GLSL, VERT_SRC, FRAG_SRC)
-        self.base_np.set_shader(shader)
+        self.pairs = []
+        self.active = False
 
-    def show(self):
         mat = p3d.Material()
 
         for building in self.city.buildings:
@@ -74,13 +72,45 @@ class CityMap(object):
 
             node = mesh_to_p3d_node(mesh, "border", mat)
             np = self.base_np.attach_new_node(node)
-            c = [c/255.0 for c in RESOURCES[building.resource].color]
-            np.set_shader_input("color", p3d.VBase3F(*c))
+            np.set_shader(shader)
             np.set_pos(pos)
 
+            self.pairs.append((building, np))
+
+        # Player Marker
+        verts = []
+        verts.append(Vertex((-0.015, -0.025, 0)))
+        verts.append(Vertex((0.015, -0.025, 0)))
+        verts.append(Vertex((0, 0.025, 0)))
+        mesh = Mesh(verts, ((0, 1, 2),))
+        node = mesh_to_p3d_node(mesh, "player_marker", mat)
+        self.player_marker = self.base_np.attach_new_node(node)
+
+    def show(self):
+        def map_task(task):
+            if not self.active:
+                return task.done
+            for pair in self.pairs:
+                building = pair[0]
+                np = pair[1]
+
+                color = RESOURCES[building.resource].color
+                color = [c/255.0 for c in color]
+                np.set_shader_input("color", p3d.VBase3F(*color))
+
+            pos = base.player_controller.player.get_pos()
+            pos[0] /= self.city.width * 0.5
+            pos[1] /= self.city.height * 0.5
+            pos[2] = 0
+            self.player_marker.set_hpr(base.player_controller.player.get_hpr())
+            self.player_marker.set_pos(pos)
+
+            return task.cont
+
         self.base_np.reparent_to(base.aspect2d)
+        self.active = True
+        base.taskMgr.add(map_task, 'Map Update')
 
     def hide(self):
-        for child in self.base_np.get_children():
-            child.remove_node()
         self.base_np.detach_node()
+        self.active = False
